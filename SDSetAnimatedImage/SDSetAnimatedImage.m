6c96ac1fe5382dc40c9b5cd466fd738e0117a451
JobsTorvalds <Wiseb2b@wiseb2b002.local>
2016-07-28 10:56:38 +0800

    Initial Commit

---
+++ SDSetAnimatedImage/SDSetAnimatedImage.m
@@ -0,0 +1,13 @@
+//
+//  SDSetAnimatedImage.m
+//  SDSetAnimatedImage
+//
+//  Created by Wiseb2b002 on 16/7/28.
+//  Copyright © 2016年 JobsTorvalds. All rights reserved.
+//
+
+#import "SDSetAnimatedImage.h"
+
+@implementation SDSetAnimatedImage
+
+@end
